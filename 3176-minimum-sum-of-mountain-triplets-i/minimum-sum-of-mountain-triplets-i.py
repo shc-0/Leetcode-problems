aec80910751b948155d23d968a1579e9d201feff
Shashwat Chouhan <80691054+shc-0@users.noreply.github.com>
2023-10-25 10:55:57 +0530

    Time: 49 ms (81.94%) | Memory: 16.1 MB (97.51%) - LeetSync

--- 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
+++ 3176-minimum-sum-of-mountain-triplets-i/minimum-sum-of-mountain-triplets-i.py
@@ -1,12 +1,24 @@
 class Solution:
     def minimumSum(self, nums: List[int]) -> int:
-        ans=float('inf')
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    if nums[i]<nums[j] and nums[k]<nums[j]:
-                        ans=min(ans,nums[i]+nums[j]+nums[k])
-        return ans if ans!=float('inf') else -1
+        if (len(nums) < 3):
+            return -1 # mountain not posible if no. of elements in nums are less than 3
+
+        min_sum = float('inf') # so that in first iteration, min_sum will be declared with sum of first mountain found
+
+        for i in range(1, len(nums) - 1):
+            # slice the left side of nums from 0 to 1, then store the min element in it
+            min_in_left_side = min(nums[:i])
+
+            # do same for the right side
+            min_in_right_side = min(nums[i+1:])
+
+            # compare the sum of current mountian with the previous mountain
+            # set sum with the mountain whose sum is least
+            if ((nums[i] > min_in_left_side) and (nums[i] > min_in_right_side)):
+                min_sum = min(min_sum, nums[i] + min_in_left_side + min_in_right_side)
+
+        # if min_sum is still infinity that means we have not found any mountain in out input
+        return min_sum if (min_sum != float('inf')) else -1
 
 
 
